b6ef1e599a63a4e4f26e588cac1a7e8e454209a0
json1989 <109114737+json19893@users.noreply.github.com>
2023-03-27 15:56:13 -0600

    snmp app in eurecka

--- SMNPV2/src/main/java/totalplay/snmpv2/com/Snmpv2Application.java
+++ SMNPV2/src/main/java/totalplay/snmpv2/com/Snmpv2Application.java
@@ -2,12 +2,14 @@ package totalplay.snmpv2.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.slf4j.Slf4j;
 @EnableScheduling
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class Snmpv2Application {
 
 	public static void main(String[] args) {
