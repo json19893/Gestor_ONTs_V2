83df91462951b6c3a92cf76c0afcc06e124dcc3e
json1989 <109114737+json19893@users.noreply.github.com>
2023-03-27 13:06:55 -0600

    eurecka

--- SMNPV2/src/main/java/totalplay/snmpv2/com/presentacion/DescubrimientoController.java
+++ SMNPV2/src/main/java/totalplay/snmpv2/com/presentacion/DescubrimientoController.java
@@ -66,7 +66,6 @@ public class DescubrimientoController extends Constantes {
 	IBitacoraEventosRepository ibitacoraEventos;
 	@Autowired
 	ItblDescubrimientoManualRepositorio descubrimientoManual;
-	@Autowired
 
 	private Integer valMaxOlts = 50;
 	String idProceso="";
