6ef587d5043e2061a5516ee2b3b8a7f0dd5c67ca
dvaldez <danielvaldez7.tr@gmail.com>
2023-03-27 16:52:27 -0600

    Se creó el helper para desencriptar los uri de conexion del archivos application.properties

---
+++ SMNPV2/src/main/java/totalplay/snmpv2/com/configuracion/MongoConnection.java
@@ -0,0 +1,49 @@
+package totalplay.snmpv2.com.configuracion;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import totalplay.snmpv2.com.helper.EncryptorHelper;
+
+@Configuration
+public class MongoConnection extends AbstractMongoClientConfiguration {
+    @Value("${spring.data.mongodb.uri}")
+    private String uriConnection;
+
+    public String getUriConnection() {
+        return uriConnection;
+    }
+
+    @Autowired
+    ApplicationContext apc;
+    private String db = "totalplayDb";
+
+    @Override
+    protected String getDatabaseName() {
+        return db;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        EncryptorHelper encryptorHelper = EncryptorHelper.getINSTANCE();
+        String uriDesencripter = encryptorHelper.deencryptString(getUriConnection());
+
+        //String uri = "mongodb://localhost:27017/";
+        System.out.println("uri encriptada: " + getUriConnection());
+        System.out.println("uri desencriptada: " + uriDesencripter);
+        //System.out.println("texto encriptado: " + encryptorHelper.encryptString("mongodb://superAdmin:pass1234@10.71.46.58:27017/"));
+
+        if (uriDesencripter.isEmpty() && uriDesencripter == null) {
+            new Exception("La uri para la conexion de mongo esta vacia");
+        }
+
+        MongoClient client = MongoClients.create(uriDesencripter);
+        //ListDatabasesIterable<Document> databases = client.listDatabases();
+        //databases.forEach(System.out::println);
+        return client;
+    }
+}
---
+++ servicios/src/main/java/totalplay/services/com/configuracion/MongoConnection.java
@@ -0,0 +1,52 @@
+package totalplay.services.com.configuracion;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import totalplay.services.com.helper.EncryptorHelper;
+
+
+@Configuration
+public class MongoConnection extends AbstractMongoClientConfiguration {
+    @Value("${spring.data.mongodb.uri}")
+    private String uriConnection;
+
+    public String getUriConnection() {
+        return uriConnection;
+    }
+
+    @Autowired
+    ApplicationContext apc;
+    private String db = "totalplayDb";
+
+    @Override
+    protected String getDatabaseName() {
+        return db;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        EncryptorHelper encryptorHelper = EncryptorHelper.getINSTANCE();
+        String uriDesencripter = encryptorHelper.deencryptString(getUriConnection());
+
+        //String uri = "mongodb://localhost:27017/";
+        System.out.println("uri encriptada: " + getUriConnection());
+        System.out.println("uri desencriptada: " + uriDesencripter);
+        //String textx = "mongodb://superAdmin:pass1234@10.180.199.76:27017/";
+        //System.out.println("texto a encriptar: " + textx);
+        //System.out.println("texto encriptado: " + encryptorHelper.encryptString(textx));
+
+        if (uriDesencripter.isEmpty() && uriDesencripter == null) {
+            new Exception("La uri para la conexion de mongo esta vacia");
+        }
+
+        MongoClient client = MongoClients.create(uriDesencripter);
+        //ListDatabasesIterable<Document> databases = client.listDatabases();
+        //databases.forEach(System.out::println);
+        return client;
+    }
+}
