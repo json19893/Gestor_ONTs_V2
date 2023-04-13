b64b3bd235b744f5d0f05d503ab14e24661c14d1
4legria <64882766+4legria@users.noreply.github.com>
2023-04-13 15:52:04 -0600

    ajuste consulta de metricas

--- SNMP_MONITOR/src/main/java/totalplay/monitor/snmp/com/persistencia/repository/IpoleoProfNameRepositorio.java
+++ SNMP_MONITOR/src/main/java/totalplay/monitor/snmp/com/persistencia/repository/IpoleoProfNameRepositorio.java
@@ -1,5 +1,6 @@
 package totalplay.monitor.snmp.com.persistencia.repository;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,11 @@ import totalplay.monitor.snmp.com.persistencia.entidad.poleosProfNameEntidad;
 
 @Repository
 public interface IpoleoProfNameRepositorio extends MongoRepository<poleosProfNameEntidad, String> {
-	@Query(value="{'id_ejecucion': ?0, 'id_olt': ?1, 'oid': ?2, }")
+	//@Query(value="{'id_ejecucion': ?0, 'id_olt': ?1, 'oid': ?2, }")
+	@Aggregation(pipeline = { 
+			"{'$match':{'$and':[{'id_ejecucion': ?0},{'id_olt': ?1}]}}"
+			,"{$sort:{_id:-1}}"
+			,"{$limit:1}"
+			})
 	poleosProfNameEntidad getMetrica(@Param("idEjecucion") String idEjecucion, @Param("idOlt") Integer idOlt, @Param("oid") String oid );
 }
