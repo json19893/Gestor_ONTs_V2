451b42e223f6506b03b8db92db49fb1e79ea9b5d
json1989 <109114737+json19893@users.noreply.github.com>
2023-04-18 14:41:15 -0600

    pipe

--- SNMP_FRONT/src/app/pipes/date-format.pipe.ts
+++ SNMP_FRONT/src/app/pipes/date-format.pipe.ts
@@ -4,10 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateFormat'
 })
 export class DateFormatPipe implements PipeTransform {
-  transform(value: any): any {
-    if(value!=null){
-    const date = new Date(value);
-    const day = date.getDate();
+  transform(date: Date): string {
+   
+   
+    if(isNaN(date.getTime())){
+      return "---"
+    }else{
+      const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
@@ -15,8 +18,8 @@ export class DateFormatPipe implements PipeTransform {
     const minutes = date.getMinutes();
 
     return `${day}/${month}/${year} ${hours}:${minutes < 10 ? '0' + minutes : minutes} ${ampm}`;
-    }else{
-        return "";
-    }
+  }
+   
+   
   }
 }
