758f0a0cac38620573568bd287d91d07d7b23783
json1989 <109114737+json19893@users.noreply.github.com>
2023-04-25 11:50:33 -0600

    ajsute de corss

--- SMNPV2/src/main/java/totalplay/snmpv2/com/helper/WebConfig.java
+++ SMNPV2/src/main/java/totalplay/snmpv2/com/helper/WebConfig.java
@@ -1,6 +1,13 @@
 package totalplay.snmpv2.com.helper;
 
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,4 +20,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Bean
+CorsConfigurationSource corsConfigurationSource() {
+CorsConfiguration configuration = new CorsConfiguration();
+configuration.setAllowedOrigins(Arrays.asList("/**"));
+configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+ // Applying the CORS to all endpoints
+ source.registerCorsConfiguration("/**", configuration);
+return (CorsConfigurationSource) source;
+}
 }
--- SNMP_MONITOR/src/main/java/totalplay/monitor/snmp/com/helper/WebConfig.java
+++ SNMP_MONITOR/src/main/java/totalplay/monitor/snmp/com/helper/WebConfig.java
@@ -1,9 +1,17 @@
 package totalplay.monitor.snmp.com.helper;
 
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Configuration
 @EnableWebMvc
@@ -13,4 +21,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("/**"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+     // Applying the CORS to all endpoints
+     source.registerCorsConfiguration("/**", configuration);
+    return (CorsConfigurationSource) source;
+}
 }
