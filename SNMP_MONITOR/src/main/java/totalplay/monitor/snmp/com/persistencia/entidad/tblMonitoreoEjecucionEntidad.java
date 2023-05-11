1366a2bec1b2d05aa2b09425deec2bb630f58703
json1989 <109114737+json19893@users.noreply.github.com>
2023-05-10 19:04:29 -0600

    ajuste de fechas

--- SNMP_MONITOR/src/main/java/totalplay/monitor/snmp/com/persistencia/entidad/tblMonitoreoEjecucionEntidad.java
+++ SNMP_MONITOR/src/main/java/totalplay/monitor/snmp/com/persistencia/entidad/tblMonitoreoEjecucionEntidad.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Document(collection = "getFecha")
 @Data
@@ -16,8 +16,8 @@ public class tblMonitoreoEjecucionEntidad {
 	@Id
 	private String id;
 	private String descripcion;
-	private LocalDateTime fecha_inicio;
-	private LocalDateTime fecha_fin;
+	private Date fecha_inicio;
+	private Date fecha_fin;
 	private Integer estatus;
 
 	
