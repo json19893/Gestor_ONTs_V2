9d28b84e5ca07240622f58c6b52a69df32073f57
json1989 <109114737+json19893@users.noreply.github.com>
2023-06-01 03:40:56 -0600

    ajuste modules

--- SNMP_FRONT/src/app/app.module.ts
+++ SNMP_FRONT/src/app/app.module.ts
@@ -34,7 +34,6 @@ import { ConvertStatusOntPipe } from './pipes/convert-status-ont.pipe';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { OntComponentDialog } from './consultaOlts/components/ont/ont.component';
 import { DialogInventarioComponent } from './consultaOlts/components/dialog-inventario/dialog-inventario.component';
 @NgModule({
